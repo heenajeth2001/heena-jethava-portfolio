7c7ea114b9b9cbe2d989f5c070e48fe94a8d6b0d
heenajeth2001 <heenajeth2001@gmail.com>
2024-06-29 14:19:00 +0530

    UI Changes commit

--- static/js/activeLink.js
+++ static/js/activeLink.js
@@ -30,19 +30,20 @@
         let activeMenuItem = localStorage.getItem("activeMenuItem");
     
         function updateActiveLinkStyles() {
+            const currentURL = window.location.href;
             menuItems.forEach(function(item) {
                 item.classList.remove("bg-gray-900", "bg-teal-700", "text-white");
             });
     
             if (activeMenuItem) {
                 const activeItem = document.querySelector(`.menu-item[href="${activeMenuItem}"]`);
-                if (activeItem) {
+                if (activeItem && activeItem.href === currentURL) {
                     if (bodyElement.classList.contains("dark")) {
-                        activeItem.classList.add("bg-teal-700", "text-white");
+                      activeItem.classList.add("bg-teal-700", "text-white");
                     } else {
-                        activeItem.classList.add("bg-gray-900", "text-white");
+                      activeItem.classList.add("bg-gray-900", "text-white");
                     }
-                }
+                  }
             }
         }
     
@@ -55,8 +56,6 @@
             item.addEventListener("click", function(event) {
                 event.preventDefault(); 
                 const url = this.getAttribute("href");
-                
-                console.log("Menu item clicked", url);
                 localStorage.setItem("activeMenuItem", url);
                 window.location.href = url; // Update the URL
             });
